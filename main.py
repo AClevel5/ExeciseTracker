647af7080e3de5c7c7448e5167fd61adb28683ef
Alex Cleveland <alexcleveland5@gmail.com>
2026-03-07 09:02:50 -0700

    Initial call to API to get exercise data back

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import os
+import requests
 
-api_key = os.environ.get("API_KEY")
-app_id = os.environ.get("APP_ID")
+BASE_URL = "https://app.100daysofpython.dev"
+API_KEY = os.environ.get("API_KEY")
+APP_ID = os.environ.get("APP_ID")
 
+headers = {
+    "Content-Type": "application/json",
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY
+}
+
+data = {"query": input("State your exercise")}
+
+response = requests.post(f"{BASE_URL}/v1/nutrition/natural/exercise", json=data, headers=headers)
+print(response.json())
